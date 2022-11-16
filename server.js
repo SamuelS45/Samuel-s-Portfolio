7b695d2fba635fda828e4e24f0b1cfba718bd46d
SamuelS45 <104377311+SamuelS45@users.noreply.github.com>
2022-11-17 02:21:58 +0700

    update API

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const express = require('express')
 
 const app = express();
 app.get('/api', (req, res)=>{
-    res.send('This API is working on Domain Name...')
+    res.send('This API is not working on Domain Name...')
 })
 app.listen(3000, ()=>console.log('Server Running on Sam Portfolio Site'))
